48a5df68e3f1066473e5d504b61b9c00791eb827
ajay-creator <ajayveeraveni119@gail.com>
2021-07-25 23:59:47 +0530

    updated design

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function myFunction() {
     myVar = setTimeout(showPage, 4000);
-  }
+}
   
 function showPage() {
 document.getElementById("loader").style.display = "none";
@@ -40,135 +40,123 @@ socialMediaBar.addEventListener("click",() => {
 });
 
 
-var number1 = document.getElementById("skill1_no");
-let counter1 = 0;
-setInterval(()=>{
-    if(counter1==80){
-        clearInterval();
-    }
-    else{
-        counter1+=1;
-        number1.textContent = counter1 + "%";
-    }
-},88)
+function isIntoView(elem)
+{
+    var documentViewTop = $(window).scrollTop();
+    var documentViewBottom = documentViewTop + $(window).height();
 
-var number2 = document.getElementById("skill2_no");
-let counter2 = 0;
-setInterval(()=>{
-    if(counter2==95){
-        clearInterval();
-    }
-    else{
-        counter2+=1;
-        number2.textContent = counter2 + "%";
-    }
-},80);
+    var elementTop = $(elem).offset().top;
+    var elementBottom = elementTop + $(elem).height();
 
-var number3 = document.getElementById("skill3_no");
-let counter3 = 0;
-setInterval(()=>{
-    if(counter3 == 50){
-        clearInterval();
-    }
-    else{
-        counter3+=1;
-        number3.textContent = counter3 + "%";
-    }
-},85);
-
-var number4 = document.getElementById("skill4_no");
-let counter4 = 0;
-setInterval(()=>{
-    if(counter4 == 75){
-        clearInterval();
-    }
-    else{
-        counter4+=1;
-        number4.textContent = counter4 + "%";
-    }
-},90);
-
-var number5 = document.getElementById("skill5_no");
-let counter5 = 0;
-setInterval(()=>{
-    if(counter5 == 90){
-        clearInterval();
-    }
-    else{
-        counter5+=1;
-        number5.textContent = counter5 + "%";
-    }
-},85);
-
-
-var project = document.getElementsByClassName("project");
-
-project[0].addEventListener("mouseover",() => {
-    
-    var details = document.getElementsByClassName("details");
-    details[0].classList.add("active");
-    var name = document.getElementsByClassName("name")[0];
-    name.textContent = ""
-
-});
-project[0].addEventListener("mouseout",() => {
-    var details = document.getElementsByClassName("details");
-    details[0].classList.remove("active");
-    var name = document.getElementsByClassName("name")[0];
-    name.textContent = "FSAE Chassis"
-});
-
-project[1].addEventListener("mouseover",() => {
-    
-    var details = document.getElementsByClassName("details");
-    details[1].classList.add("active");
-    var name = document.getElementsByClassName("name")[2];
-    name.textContent = ""
-
-});
-project[1].addEventListener("mouseout",() => {
-    var details = document.getElementsByClassName("details");
-    details[1].classList.remove("active");
-    var name = document.getElementsByClassName("name")[2];
-    name.textContent = "Portfolio"
-});
-
-
-project[2].addEventListener("mouseover",() => {
-    
-    var details = document.getElementsByClassName("details");
-    details[2].classList.add("active");
-    var name = document.getElementsByClassName("name")[4];
-    name.textContent = ""
-
-});
-project[2].addEventListener("mouseout",() => {
-    var details = document.getElementsByClassName("details");
-    details[2].classList.remove("active");
-    var name = document.getElementsByClassName("name")[4];
-    name.textContent = "FLY"
-});
-
-
-project[3].addEventListener("mouseover",() => {
-    
-    var details = document.getElementsByClassName("details");
-    details[3].classList.add("active");
-    var name = document.getElementsByClassName("name")[6];
-    name.textContent = ""
-
-});
-project[3].addEventListener("mouseout",() => {
-    var details = document.getElementsByClassName("details");
-    details[3].classList.remove("active");
-    var name = document.getElementsByClassName("name")[6];
-    name.textContent = "E commerce"
-});
-
+    return ((elementBottom <= documentViewBottom) && (elementTop >= documentViewTop));
 }
+let count1=count2=count3=count4=count5=count6=1;
+const progress = document.querySelectorAll(".progress");
+
+$(window).scroll(function(){
+   if (isIntoView($('.skill1')) && count1){
+    var number1 = document.getElementById("skill1_no");
+    let counter1 = 0;
+    progress[0].style.display = "block";
+    progress[1].style.display = "block";
+    setInterval(()=>{
+        if(counter1==90){
+            clearInterval();
+        }
+        else{
+            counter1+=1;
+            number1.textContent = counter1 + "%";
+        }
+    },88)
+    count1=0;
+}
+if (isIntoView($('.skill2'))&& count2){
+    var number2 = document.getElementById("skill2_no");
+    let counter2 = 0;
+    progress[2].style.display = "block";
+    progress[3].style.display = "block";
+    setInterval(()=>{
+        if(counter2==75){
+            clearInterval();
+        }
+        else{
+            counter2+=1;
+            number2.textContent = counter2 + "%";
+        }
+    },80);
+    count2=0;
+  }
+  if (isIntoView($('.skill3'))&&count3){
+    var number3 = document.getElementById("skill3_no");
+    let counter3 = 0;
+    progress[4].style.display = "block";
+    progress[5].style.display = "block";
+    setInterval(()=>{
+        if(counter3 == 50){
+            clearInterval();
+        }
+        else{
+            counter3+=1;
+            number3.textContent = counter3 + "%";
+        }
+    },85);
+    count3=0;
+}
+if (isIntoView($('.skill4')) && count4){
+
+    var number4 = document.getElementById("skill4_no");
+    let counter4 = 0;
+    progress[6].style.display = "block";
+    progress[7].style.display = "block";
+    setInterval(()=>{
+        if(counter4 == 50){
+            clearInterval();
+        }
+        else{
+            counter4+=1;
+            number4.textContent = counter4 + "%";
+        }
+    },85);
+    count4=0;
+}
+if (isIntoView($('.skill5')) && count5){
+    var number5 = document.getElementById("skill5_no");
+    let counter5 = 0;
+    progress[8].style.display = "block";
+    progress[9].style.display = "block";
+    setInterval(()=>{
+        if(counter5 == 90){
+            clearInterval();
+        }
+        else{
+            counter5+=1;
+            number5.textContent = counter5 + "%";
+        }
+    },88);
+    count5=0;
+}
+if (isIntoView($('.skill6')) && count6){
+    var number6 = document.getElementById("skill6_no");
+    let counter6 = 0;
+    progress[10].style.display = "block";
+    progress[11].style.display = "block";
+    setInterval(()=>{
+        if(counter6 == 90){
+            clearInterval();
+        }
+        else{
+            counter6+=1;
+            number6.textContent = counter6 + "%";
+        }
+    },88);
+    count6=0;
+}
+});
+};
 
 // Contact Form
-function SubForm (){
+
+function SubForm(){
     $.ajax({
         url:"https://api.apispreadsheets.com/data/15356/",
         type:"post",
